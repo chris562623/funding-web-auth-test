c70de7c88d2f17354937a8a4707a5a9cb0eb2473
chris562623 <chrisk111122@gmail.com>
2025-12-18 20:07:28 +0530

    up-2

--- script.js
+++ script.js
@@ -91,4 +91,24 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		});
 	}
+
+	// password toggle
+	const togglePasswordIcons = document.querySelectorAll('.toggle-password');
+
+	togglePasswordIcons.forEach(icon => {
+		icon.addEventListener('click', function () {
+			const targetId = this.dataset.target;
+			const passwordInput = document.getElementById(targetId);
+			if (passwordInput.type === 'password') {
+				passwordInput.type = 'text';
+				this.classList.remove('fa-eye');
+				this.classList.add('fa-eye-slash');
+			} else {
+				passwordInput.type = 'password';
+				this.classList.remove('fa-eye-slash');
+				this.classList.add('fa-eye');
+			}
+		});
+	});
+
 });
